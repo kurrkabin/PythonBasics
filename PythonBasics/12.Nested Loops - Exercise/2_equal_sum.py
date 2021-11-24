966a86be568186b0d0e236e540deea873d773a1a
kurrkabin <40040554+kurrkabin@users.noreply.github.com>
2021-11-24 10:53:35 +0200

    Update 2_equal_sum.py

--- PythonBasics/12.Nested Loops - Exercise/2_equal_sum.py
+++ PythonBasics/12.Nested Loops - Exercise/2_equal_sum.py
@@ -15,11 +15,7 @@ for current_number in range(first_number, second_number + 1):
 
 #end = ' ', it gave me 100%
 
-#Notes:
-#1.if you place odd_digit_sum and even_digit_sum outside the for-loop, it calculates the odd/even sum for overall w/o resetting for each new number, this way
-# you just see the end result of the sum of all odd vs even numbers. Instead, the sum should reset with each new current number.
 
-#2.Why do we need to have the last "if odd_digit_sum == even_digit_sum outside the inner loop, but inside the first loop?
-#-If we leave it inside the inner circle, it will print a number the moment it equalizes even if there are more digits to come. If it stays outside the inner circle, it
-#only does it when the number is complete.
+
+
 
