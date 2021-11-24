25ae56f35f281d3931b011ce358fabe806b5007b
kurrkabin <40040554+kurrkabin@users.noreply.github.com>
2021-11-24 10:52:43 +0200

    Update 6_building.py

--- PythonBasics/11.Nested Loops - Lab/6_building.py
+++ PythonBasics/11.Nested Loops - Lab/6_building.py
@@ -18,24 +18,4 @@ for x in range(floors, 0, -1):
             print(flat_name, end='')
     print()
 
-# My decision. Works very well
-# floors = int(input())
-# flats_per_floor = int(input())
-#
-# flat_name = ''
-#
-# for x in range(floors, 0, -1):
-#     for j in range(flats_per_floor):
-#         flat_number = x * 10 + j
-#         if x == floors:
-#             print(f'L{flat_number} ', end='')
-#         elif x % 2 != 0:
-#             flat_name += f'A{flat_number} '
-#         else:
-#             flat_name += f'O{flat_number} '
-#
-#     print(flat_name)
-#     flat_name = ''
-#
-# #I just copy-pasted it. This took me 2 fucking hours to solve.
-#
+
